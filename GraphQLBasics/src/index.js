14c9d2e194c6f95199959a446812e37c25010683
Tina <tanjilla.tina@gmail.com>
2021-07-25 11:54:09 +0600

    schema basics

--- GraphQLBasics/src/index.js
+++ GraphQLBasics/src/index.js
@@ -1,4 +1,38 @@
 import { GraphQLServer } from 'graphql-yoga'
-//Type Definitions
-
+/*Type Definitions
+also known as GraphQL Schemas 
+*/
+const typeDefs=` type Query {
+    hello : String!
+    name: String!
+    location: String!
+    bio: String!
+    
+}`;
 //Resolvers
+const resolvers ={
+Query :{
+    hello(){
+        return `This is my first query`
+    },
+    name(){
+       return 'Tina' 
+    },
+    location(){
+        return 'Dhaka'
+    },
+    bio(){
+        return "Whatever"
+    }
+}
+};
+const server=new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(()=>{
+    console.log('Server is running')
+});
+
+//Server running at port 4000 by default
---
+++ GraphQLBasics/src/myModule.js
@@ -0,0 +1,19 @@
+const message="mesage from mymodule"
+const messageOwner="Tina"
+const myCurrentlocation="Dhaka"
+
+const getGreeting =(name)=>{
+    return `Hello from ${name}` ;
+}
+
+
+export {message, messageOwner, myCurrentlocation as default,
+    getGreeting};
+/*
+Named Export - 
+     a//has specific name to export,
+     b//Have as many as needed
+Default Export -
+     a// has no name
+     b// you can use only one
+*/
