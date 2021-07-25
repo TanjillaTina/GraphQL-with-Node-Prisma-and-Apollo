c906ca2afaaeff6690599efb8b6e0a671ddb5ec4
Tina <tanjilla.tina@gmail.com>
2021-07-25 11:01:59 +0600

    added  export imports

--- GraphQLBasics/src/index.js
+++ GraphQLBasics/src/index.js
@@ -1,1 +1,5 @@
 console.log("IN THE NAME OF ALLAH,THE BENEFICENT AND THE MERCIFUL")
+import myCurrentlocation, {message, messageOwner, getGreeting} from "./myModule";
+console.log(`${message} ${messageOwner} Location is ${myCurrentlocation}`);
+
+console.log(`from getGreeting ${getGreeting("Tanjilla")}`)
